b413df71e471033fb4030f56c828929f0eb453d9
Vincent Meron <vmeron@vincemac2.local>
2015-12-09 21:34:54 +0100

    Wip on phpfarm

---
+++ compose/phpfarm/custom-options.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+
+configoptions="\
+--with-libdir=/lib/x86_64-linux-gnu \
+--enable-memory-limit \
+--with-regex=php \
+--enable-sysvsem \
+--enable-sysvshm \
+--enable-sysvmsg \
+--enable-track-vars \
+--enable-trans-sid \
+--with-bz2 \
+--enable-ctype \
+--without-gdbm \
+--with-iconv \
+--enable-filepro \
+--enable-shmop \
+--with-libxml-dir=/usr \
+--with-kerberos=/usr \
+--with-openssl \
+--enable-dbx \
+--with-system-tzdata \
+--with-mysql=/usr/bin/mysql_config \
+--with-mysqli=/usr/bin/mysql_config \
+--enable-pdo \
+--with-pdo-mysql=shared \
+--enable-fastcgi \
+--enable-force-cgi-redirect \
+--with-curl \
+--enable-bcmath \
+--enable-calendar \
+--enable-exif \
+--enable-ftp \
+--with-gd \
+--with-jpeg-dir=/usr \
+--with-png-dir \
+--with-freetype-dir=/usr \
+--with-t1lib \
+--with-zlib-dir=/usr \
+--with-gettext=/usr \
+--enable-mbstring \
+--with-mcrypt=/usr \
+--with-mhash \
+--with-mime-magic \
+--enable-soap \
+--enable-sockets \
+--with-tidy \
+--enable-wddx \
+--with-xsl=/usr \
+--with-zip \
+--with-zlib \
+--enable-zip"
